14dab493da6bb90089fe698e05d2df4bdb450682
Jessica Orihuela Rojas <jessica.orihuela.rojas@gmail.com>
2024-07-18 16:29:14 -0400

    Uploading project.

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ titulo.innerHTML = 'Guess the number!';
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Guess the number between 1 - 10';
+
+function intentoDeUsuario(){
+    let numeroUsuario = document.querySelector('input').value;
+    let numeroAleatorio = Math.floor(Math.random()*10 + 1);
+    // console.log(numeroAleatorio);
+    if (numeroUsuario == numeroAleatorio){
+        parrafo.innerHTML = 'You guessed the number!';
+    } else {
+        parrafo.innerHTML = 'Wrong number, try again!';
+    }
+}
