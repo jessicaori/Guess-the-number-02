cf835b4ba1da5ec8555d0681673c5f709ece9fac
Jessica Orihuela Rojas <jessica.orihuela.rojas@gmail.com>
2024-07-18 15:40:02 -0400

    Changing colors.

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Guess the number!';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Guess the number between 1 - 10';
