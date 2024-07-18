63b5416ee5fee4c5ae43bf73adfc0560d5691a8b
Jessica Orihuela Rojas <jessica.orihuela.rojas@gmail.com>
2024-07-18 17:05:52 -0400

    Working on logic. Game works for 1 secret number.

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 function asignarTextoElemento(elemento,texto){
     let text = document.querySelector(elemento);
     text.innerHTML = texto;
+    return;
 }
 
-function intentoDeUsuario(){
-    let numeroUsuario = document.querySelector('input').value;
-    let numeroAleatorio = Math.floor(Math.random()*10 + 1);
-    // console.log(numeroAleatorio);
+function generarNumeroSecreto(){
+    return Math.floor(Math.random()*10)+1;
+}
+
+function verificarIntento(){
+    let numeroUsuario = parseInt(document.getElementById("valorUsuario").value);
+    console.log(typeof(numeroUsuario));
+    console.log(numeroAleatorio);
     if (numeroUsuario == numeroAleatorio){
-        parrafo.innerHTML = 'You guessed the number!';
+        alert('Congrats! You guessed the number!');
     } else {
-        parrafo.innerHTML = 'Wrong number, try again!';
+        if(numeroUsuario < numeroAleatorio){
+            msg = "The number is greater!"
+        }else{
+            msg = "The number is less!"
+        }
+        alert('Wrong number, try again! ' + msg);
     }
+    return;
 }
 
 asignarTextoElemento('h1', 'Guess the number!');
 asignarTextoElemento('p', 'Guess the number between 1 - 10');
+
+let numeroAleatorio = generarNumeroSecreto();
